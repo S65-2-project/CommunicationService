41f8375a3a7272f7647ba3564b8690f4b5709e0d
Stijn <baltessen.stijn@gmail.com>
2020-06-12 10:52:37 +0200

    Feature/s65 211 (#11)
    * s65-211: added jwt to gpg
    
    * s65-211: Implmented jwt helper

---
+++ CommunicationService/Helper/JwtIdClaimReaderHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommunicationService.Helper
+{
+    public class JwtIdClaimReaderHelper : IJwtIdClaimReaderHelper
+    {
+        public Guid getUserIdFromToken(string jwt)
+        {
+            var token = new JwtSecurityToken(jwt.Replace("Bearer ", String.Empty));
+            var idclaim = Guid.Parse((string)token.Payload["unique_name"]);
+            return idclaim;
+        }
+    }
+}
